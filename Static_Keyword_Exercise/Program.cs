331563e2683d10768a7548f1e00dd125d0a543c7
johndward01 <johndward01@gmail.com>
2023-07-19 11:56:28 -0500

    Add project files.

---
+++ Static_Keyword_Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿// TODO: Create a person class
+// TODO: Inside the person class create a non-static field called name
+// TODO: Inside the person class create a static field called count
+// TODO: Inside the person class modify the constructor to increment the count field
+//       each time a Person object is created
+// TODO: Inside the person class create a static method that prints 
+//       the total number of Person objects created   
+
+
+// TODO: Create a for-loop that creates Person objects 10 times 
+
+// TODO: Call the static method to print out the count creatively
