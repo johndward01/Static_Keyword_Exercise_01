4e996f9bc1c09bcb5fe09d3b9e84c7ae900ee995
johndward01 <johndward01@gmail.com>
2023-07-19 12:05:54 -0500

    update

---
+++ Static_Keyword_Exercise/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace Static_Keyword_Exercise;
+
+public class Person
+{
+    public static int count;
+    public string name;
+
+    public Person()
+    {
+        count++;
+    }
+
+    public static void PrintCount()
+    {
+        Console.WriteLine($"People Count: {count}");
+    }
+
+}
--- Static_Keyword_Exercise/Program.cs
+++ Static_Keyword_Exercise/Program.cs
@@ -1,12 +1,19 @@
-﻿// TODO: Create a person class
+﻿using Static_Keyword_Exercise;
+// TODO: Create a person class
 // TODO: Inside the person class create a non-static field called name
 // TODO: Inside the person class create a static field called count
 // TODO: Inside the person class modify the constructor to increment the count field
 //       each time a Person object is created
-// TODO: Inside the person class create a static method that prints 
-//       the total number of Person objects created   
+// TODO: Inside the person class create a static method called PrintCount() that prints 
+//       the total number of Person objects   
 
 
-// TODO: Create a for-loop that creates Person objects 10 times 
 
-// TODO: Call the static method to print out the count creatively
+// TODO: Create a for-loop that creates 10 Person objects 
+for (int i = 0; i < 10; i++)
+{
+    var person = new Person();
+}
+
+// TODO: Call the static method to print out the count 
+Person.PrintCount();
