389c5fb25a8cafc0e1a812c308e94f8c8dddc1c4
johndward01 <johndward01@gmail.com>
2023-07-19 12:10:01 -0500

    update

--- Static_Keyword_Exercise/Program.cs
+++ Static_Keyword_Exercise/Program.cs
@@ -3,10 +3,11 @@
 // TODO: Inside the person class create a static field called count
 // TODO: Inside the person class modify the constructor to increment the count field
 //       each time a Person object is created
-// TODO: Inside the person class create a static method that prints 
-//       the total number of Person objects created   
+// TODO: Inside the person class create a static method called PrintCount() that prints 
+//       the total number of Person objects   
 
 
-// TODO: Create a for-loop that creates Person objects 10 times 
 
-// TODO: Call the static method to print out the count creatively
+// TODO: Create a for-loop that creates 10 Person objects 
+
+// TODO: Call the static method to print out the count 
